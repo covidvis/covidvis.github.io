7d7a5a62094ab57ab3c1f5ff0262200e8760d3b4
Stephen Macke <smacke@illinois.edu>
2020-04-28 15:08:33 -0700

    deploy

--- js/autogen/vega_embed.js
+++ js/autogen/vega_embed.js
@@ -1,15 +1,48 @@
 
+var COVIDVIS_CHARTS = {};
 function startVegaEmbedding() {
   var embedOpt = {"mode": "vega-lite"};
   $(document).ready(function() {
-    vegaEmbed("#jhu_world_cases", jhu_world_cases, embedOpt);
-    vegaEmbed("#jhu_world_deaths", jhu_world_deaths, embedOpt);
-    vegaEmbed("#jhu_us_cases", jhu_us_cases, embedOpt);
-    vegaEmbed("#jhu_us_deaths", jhu_us_deaths, embedOpt);
-    vegaEmbed("#jhu_world_cases_mobile", jhu_world_cases_mobile, embedOpt);
-    vegaEmbed("#jhu_world_deaths_mobile", jhu_world_deaths_mobile, embedOpt);
-    vegaEmbed("#jhu_us_cases_mobile", jhu_us_cases_mobile, embedOpt);
-    vegaEmbed("#jhu_us_deaths_mobile", jhu_us_deaths_mobile, embedOpt);
+vegaEmbed("#jhu_world_cases", jhu_world_cases, embedOpt).then(function(chart) {
+    COVIDVIS_CHARTS['jhu_world_cases'] = chart;
+    
+});
+vegaEmbed("#jhu_world_deaths", jhu_world_deaths, embedOpt).then(function(chart) {
+    COVIDVIS_CHARTS['jhu_world_deaths'] = chart;
+    
+});
+vegaEmbed("#jhu_us_cases", jhu_us_cases, embedOpt).then(function(chart) {
+    COVIDVIS_CHARTS['jhu_us_cases'] = chart;
+    
+    var handler = makePopulateInfoPageSpaceHandler('jhu_us_cases');
+    chart.view.addSignalListener('click', handler);
+    handler('click', chart.view.signal('click'));
+
+});
+vegaEmbed("#jhu_us_deaths", jhu_us_deaths, embedOpt).then(function(chart) {
+    COVIDVIS_CHARTS['jhu_us_deaths'] = chart;
+    
+    var handler = makePopulateInfoPageSpaceHandler('jhu_us_deaths');
+    chart.view.addSignalListener('click', handler);
+    handler('click', chart.view.signal('click'));
+
+});
+vegaEmbed("#jhu_world_cases_mobile", jhu_world_cases_mobile, embedOpt).then(function(chart) {
+    COVIDVIS_CHARTS['jhu_world_cases_mobile'] = chart;
+    
+});
+vegaEmbed("#jhu_world_deaths_mobile", jhu_world_deaths_mobile, embedOpt).then(function(chart) {
+    COVIDVIS_CHARTS['jhu_world_deaths_mobile'] = chart;
+    
+});
+vegaEmbed("#jhu_us_cases_mobile", jhu_us_cases_mobile, embedOpt).then(function(chart) {
+    COVIDVIS_CHARTS['jhu_us_cases_mobile'] = chart;
+    
+});
+vegaEmbed("#jhu_us_deaths_mobile", jhu_us_deaths_mobile, embedOpt).then(function(chart) {
+    COVIDVIS_CHARTS['jhu_us_deaths_mobile'] = chart;
+    
+});
   });
 };
     
